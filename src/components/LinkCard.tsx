8f0eb8cfe0c7fc72f6b0d5824e28eeeb5bf0fe40
Edema <edemaero@gmail.com>
2022-10-20 21:40:07 +0100

    updates on LinkCard and Main section with css styling

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -2,8 +2,26 @@
 
 import React from "react";
 
-const LinkCard = () => {
-  return <div>LinkCard</div>;
+type linkCardProps = {
+  title: string;
+  link: string;
+  id: string;
+  fontweight: boolean | undefined;
+};
+
+const LinkCard = ({ title, link, id, fontweight }: linkCardProps) => {
+  return (
+    <a href={link}>
+      <button
+        id={id}
+        className={`w-full py-[24px] bg-[#EAECF0] border border-[#EAECF0] rounded-[8px] text-[18px] leading-[28px] text-[#101828] ${
+          fontweight ? "font-semibold" : "font-medium"
+        }`}
+      >
+        {title}
+      </button>
+    </a>
+  );
 };
 
 export default LinkCard;
--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -3,11 +3,64 @@
 import React from "react";
 import { LinkCard } from "./index";
 
+const Links = [
+  {
+    title: "Twitter Link",
+    link: "https://twitter.com/_beyond_logic",
+    id: "twitter",
+    fontWeight: false,
+  },
+  {
+    title: "Zuri Team",
+    link: "https://training.zuri.team/",
+    id: "btn__zuri",
+    fontWeight: true,
+  },
+  {
+    title: "Zuri Books",
+    link: "http://books.zuri.team/",
+    id: "books",
+    subtext: "This is where you find books about design and coding",
+    fontWeight: true,
+  },
+
+  {
+    title: "Python Books",
+    link: "https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>",
+    id: "book__python",
+    fontWeight: true,
+  },
+
+  {
+    title: "Background Check for Coders",
+    link: "https://background.zuri.team/",
+    id: "pitch",
+    subtext: "This is where you find books about design and coding",
+    fontWeight: true,
+  },
+
+  {
+    title: "Design Books",
+    link: "https://books.zuri.team/design-rules",
+    id: "book__design",
+    subtext: "Don't let little mistakes cost you a chance",
+  },
+];
+
 const MainSection = () => {
   return (
     <div>
-      <div className="flex flex-col space-y-[24px] mt-[56px]">
-        <LinkCard />
+      <div className="flex flex-col space-y-[24px] mt-[56px] mb-[48px]">
+        {Links &&
+          Links.map((item, key) => (
+            <LinkCard
+              key={key}
+              title={item.title}
+              link={item.link}
+              id={item.id}
+              fontweight={item.fontWeight}
+            />
+          ))}
       </div>
     </div>
   );
